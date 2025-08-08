87672b3ea96bd1810f80db2dccde8e830c56895e
Augusto Moscardo <augustomsaluz@gmail.com>
2025-08-08 14:17:56 -0300

    init data loadeer

--- data-loader/load-data.js
+++ data-loader/load-data.js
@@ -3,7 +3,7 @@ import csv from "csv-parser";
 import fs from "fs";
 
 const chromaClient = new ChromaClient();
-
+await chromaClient.deleteCollection({ name: "movies" })
 const collection = await chromaClient.getOrCreateCollection({ name: "movies" });
 
 const ids = [];
@@ -11,18 +11,29 @@ const documents = [];
 const metadatas = [];
 
 fs.createReadStream("mpst_full_data.csv")
-.pipe(csv())
-.on('data', (row) => {
-    const document = {"title": row["title"], "tags": row["tags"], "synopsis": row["plot_synopsis"]};
+	.pipe(csv())
+	.on('data', (row) => {
+		const document = { "title": row["title"], "tags": row["tags"], "synopsis": row["plot_synopsis"] };
 
-    ids.push(row["imdb_id"]);
-    documents.push(JSON.stringify(document));
-    metadatas.push(document);
-})
-.on('end', async () => {
-    await collection.add({
-    ids: ids,
-    documents: documents,
-    metadatas: metadatas,
-    });
-})
+		ids.push(row["imdb_id"]);
+		documents.push(JSON.stringify(document));
+		metadatas.push(document);
+	})
+	.on('end', async () => {
+		let startIdx = 0;
+
+		while (startIdx < ids.length) {
+			let endIdx = startIdx + 500; // ajustar essa quantidade de acordo com o seu hardware, quanto maior o valor, maior o processamento necessário
+			console.log(`Adding documents from ${startIdx} to ${endIdx}`)
+
+			await collection.add({
+				ids: ids.slice(startIdx, endIdx),
+				documents: documents.slice(startIdx, endIdx),
+				metadatas: metadatas.slice(startIdx, endIdx),
+			});
+
+			startIdx = endIdx;
+		}
+
+		console.log("Done!")
+	})
